8f15bc3eac810896a8749af61680d7fe24731628
Kirthana Sukumaran <kirthanaa07@gmail.com>
2023-06-09 21:01:57 -0500

    Final commit

--- components/buttons/filterButton.js
+++
@@ -1,7 +0,0 @@
-    <div class="btn-group" role="group" aria-label="Basic mixed styles example">
-      <button type="button" class="btn btn-secondary all-btn" id="all">All</button>
-      <button type="button" class="btn btn-danger gryf-btn" id="gryffindor">Gryffindor</button>
-      <button type="button" class="btn btn-warning huff-btn" id="hufflepuff">Hufflepuff</button>
-      <button type="button" class="btn btn-primary rav-btn" id="ravenclaw">Ravenclaw</button>
-      <button type="button" class="btn btn-success slyt-btn" id="slytherin">Slytherin</button>
-    </div>
--- pages/vocabulary.js
+++ pages/vocabulary.js
@@ -44,11 +44,11 @@ const showVocabulary = (array, userId) => {
 };
 
 const myVocabularyFilterActions = (array) => {
-  let btnString = '<div class="d-flex justify-content-center gap-3"><button class="btn btn-success btn-lg mb-4" id="filter-my-btn--all">All</button>';
+  let btnString = '<div class="d-flex justify-content-center gap-3"><button class="filter-btn-css btn btn-success btn-lg mb-4" id="filter-my-btn--all">All</button>';
 
   array.forEach((item) => {
     // "--" here separates the id into 2 parts because 'filter-btn,categoryID' are 1 id.
-    btnString += `<button class="btn btn-success btn-lg mb-4" id="filter-my-btn--${item.category_id}">${item.name}</button>`;
+    btnString += `<button class="filter-btn-css btn btn-success btn-lg mb-4" id="filter-my-btn--${item.category_id}">${item.name}</button>`;
   });
   btnString += '</div>';
   btnString += `<div class="d-flex justify-content-between space-bottom"><input class="form-control mr-sm-2"
